715feb321e59fd91b20c84b2600b9f9db0c1db48
Dmitry Mozgin <m0391n@gmail.com>
2013-02-27 18:50:32 +0400

    First commit

---
+++ demo/src/com/m039/ut/demo/UWidgetsDemoActivity.java
@@ -0,0 +1,15 @@
+package com.m039.ut.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UWidgetsDemoActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
